e916ff2c08350e634573d2a848bca67e397c4fb8
Mike Sampson <mike@sambodata.com>
2019-07-05 17:36:37 +1000

    Update Planck config.

--- keyboards/planck/keymaps/mfs/config.h
+++ keyboards/planck/keymaps/mfs/config.h
@@ -1,7 +1,4 @@
-#ifndef CONFIG_USER_H
-#define CONFIG_USER_H
-
-#include "../../config.h"
+#pragma once
 
 #ifdef AUDIO_ENABLE
     #define STARTUP_SONG SONG(PLANCK_SOUND)
@@ -12,8 +9,6 @@
                                   SONG(DVORAK_SOUND) \
                                 }
 #endif
-
-#define MUSIC_MASK (keycode != KC_NO)
 
 /*
  * MIDI options
@@ -39,4 +34,6 @@
 /* override number of MIDI tone keycodes (each octave adds 12 keycodes and allocates 12 bytes) */
 //#define MIDI_TONE_KEYCODE_OCTAVES 2
 
-#endif
+// Most tactile encoders have detents every 4 stages
+#define ENCODER_RESOLUTION 4
+
